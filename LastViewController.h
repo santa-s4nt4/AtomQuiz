10d41f86860372dc387f686ebc5bf98e54d396e9
santa naruse <santanaruse@santas-MacBook-Air.local>
2014-10-25 20:11:00 +0900

    デザインが出来てきた。Facebook,twitterへ投稿できる。

--- LastViewController.h
+++ LastViewController.h
@@ -1,20 +1,22 @@
 //
-//  ViewController.h
+//  LastViewController.h
 //  Atom Quiz
 //
-//  Created by santa naruse on 2014/10/21.
+//  Created by santa naruse on 2014/10/25.
 //  Copyright (c) 2014年 santa naruse. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface LastViewController : UIViewController
+{
+    int number;
+}
 
 -(IBAction)face;
 
 -(IBAction)twitter;
 
--(IBAction)explanation;
+-(IBAction)home;
 
 @end
-
--- LastViewController.m
+++ LastViewController.m
@@ -1,29 +1,39 @@
 //
-//  ViewController.m
+//  LastViewController.m
 //  Atom Quiz
 //
-//  Created by santa naruse on 2014/10/21.
+//  Created by santa naruse on 2014/10/25.
 //  Copyright (c) 2014年 santa naruse. All rights reserved.
 //
 
-#import "ViewController.h"
+#import "LastViewController.h"
 #import <Social/Social.h>
 
-@interface ViewController ()
+@interface LastViewController ()
 
 @end
 
-@implementation ViewController
+@implementation LastViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    // Do any additional setup after loading the view.
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
 
 -(IBAction)face
 {
@@ -43,14 +53,9 @@
     [self presentViewController:twVC animated:YES completion:nil];
 }
 
--(IBAction)explanation
+-(IBAction)home
 {
-    UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"説明"
-                                                    message:@"このアプリは原子記号クイズです。"
-                                                   delegate:self
-                                          cancelButtonTitle:@"OK"
-                                          otherButtonTitles:nil];
-    [alert show];
+    [self dismissViewControllerAnimated:YES completion:nil]; //Topに戻る
 }
 
 @end
